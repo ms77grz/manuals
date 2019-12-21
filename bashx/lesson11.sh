216f7f89196eca1d76358c4eeafbe8767bb1bd0d
ms77grz <ms77grz@gmail.com>
2019-12-21 12:23:12 +0300

    ok

--- bashx/lesson11.sh
+++ bashx/lesson11.sh
@@ -3,4 +3,13 @@
 num1=20.5
 num2=5
 
-echo "20.5+5" | bc
+echo "$num1 + $num2" | bc
+echo "$num1 - $num2" | bc
+echo "$num1 * $num2" | bc
+echo "scale=2;$num1 / $num2" | bc
+echo "$num1 % $num2" | bc
+
+num3=27
+echo "scale=2;sqrt($num3)" | bc -l
+
+echo "3^3" | bc
